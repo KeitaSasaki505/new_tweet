625105f68575e35bbc450ee24bdd8bfb09fcb73c
KeitaSasaki505 <cutba505@gmail.com>
2020-10-03 17:10:26 +0900

    一時保存

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,27 +1,46 @@
 class TweetsController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:edit, :show]
+  before_action :set_tweet, only: [:edit, :show]
+
+
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.includes(:user).order("created_at DESC")
   end
 
   def new
+    @tweet = Tweet.new
   end
 
   def create
+    Tweet.create(tweet_params)
   end
 
   def edit
   end
 
   def show
+    @comment = Comment.new
+    @comments = @tweet.comments.includes(:user)
   end
 
   def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
   end
 
   def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
   end
 
+private
 
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:text).merge(user_id: current_user.id)
+  end
 
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,5 +3,5 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  
+  validates :text, presence: true
 end
